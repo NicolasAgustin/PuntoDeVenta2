516d85119acc34660a8213b0a87d1c72e08a24f2
aviljen1 <aviljen1@gmail.com>
2023-09-01 20:54:35 -0300

    cambios

---
+++ PuntoVenta/ventas/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from ventas.models import Cliente
+
+
+class AddClienteForm(forms.ModelForm):
+	class Meta:
+		model=Cliente
+		fields=('codigo', 'nombre', 'telefono')
+		labels={
+			'codigo': 'Código cliente: ',
+			'nombre': 'Nombre cliente: ',
+			'telefono': 'Telefono: (Contacto): '
+		}
+
+
+
+
+class EditarClienteForm(forms.ModelForm):
+	class Meta:
+		model=Cliente
+		fields=('codigo', 'nombre', 'telefono')
+		labels={
+			'codigo': 'Código cliente: ',
+			'nombre': 'Nombre cliente: ',
+			'telefono': 'Telefono: (Contacto): '
+		}
+
+	widgets = {
+			'codigo':forms.TextInput(attrs={'type': 'text', 'id': 'codigo_editar'}),
+			'nombre': forms.TextInput(attrs={'id': 'nombre_editar'}),
+			'telefono': forms.TextInput(attrs={'id': 'telefono_editar'}),
+		}
+	
+#class EditarClienteForm(forms.ModelForm):
+	#class Meta:
+		#imagen=forms.ImageField()
+		#model=Cliente
+		#fields=('codigo','nombre', 'telefono', 'imagen')
+		#labels={
+		#	'codigo': 'Código: ',
+		#	'telefono': 'Telefono: ',
+		#	'imagen': 'Imagen: ',
+		#	'nombre': 'Descripcion: '
+		#}
+		
+		#widgets={
+		#	'codigo': forms.TextInput(attrs={'type: 'text', 'id': 'codigo_editar'}),
+		#	'nombre': forms.TextInput(attrs={'id': 'nombre_editar'}),
+		#	'telefono': forms.TextInput(attrs={'id': 'telefono_editar'}),
+		#}
+
+
--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from ventas.models import Cliente, Producto
 
 # Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+	list_display=('nombre', 'telefono', 'codigo')
+	search_fields=['nombre']
+	#readonly_fields=('created','updated') ME DA ERRO ACA
+	#TODO
+	filter_horizontal= ()
+	list_filter=()
+	fieldsets=()
+
+admin.site.register(Cliente, ClienteAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display=('descripcion', 'cantidad', 'costo')
+	search_fields=['descripcion']
+	readonly_fields=('created', 'updated') 
+	filter_horizontal= ()
+	list_filter=()
+	fieldsets=()
+
+admin.site.register(Producto, ProductoAdmin)
