e00121fbb0bb807ea48bdf3aa2a5a24958822dac
aviljen1 <aviljen1@gmail.com>
2023-09-22 20:20:59 -0300

    da error por  libreria

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from ventas.views import ventas_view
 from clientes.views import clientes_view, add_cliente_view, edit_cliente_view, delete_cliente_view, productos_view, add_producto_view
-
+from ventas import views  # Asegúrate de importar las vistas de ventas
 
 #TODO separar url de clientes y ventas 
 urlpatterns = [
@@ -12,4 +12,7 @@ urlpatterns = [
    path('delete_cliente/', delete_cliente_view, name='DeleteCliente'),
    path('productos/', productos_view, name='Productos'),
    path('add_producto/', add_producto_view, name='AddProducto'),
+   path('add_venta/',views.add_ventas.as_view(), name='AddVenta'),
+   path('export/', views.export_pdf_view, name="ExportPDF" ),
+   path('export/<id>/<iva>', views.export_pdf_view, name="ExportPDF" ),
 ]
